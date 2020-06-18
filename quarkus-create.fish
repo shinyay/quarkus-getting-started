ffb27844209e5601befc7d824101b042f1d1c57d
shinyay <shinya.com@gmail.com>
2020-06-18 18:30:48 +0900

    Add Quarkus Project Template Generate

---
+++ quarkus-create.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+mvn io.quarkus:quarkus-maven-plugin:1.5.1.Final:create \
+      -DprojectGroupId=com.google.shinyay \
+      -DprojectArtifactId=quarkus-getting-started \
+      -DprojectVersion=0.0.1-SNAPSHOT \
+      -DclassName="com.google.shinyay.GreetingResource" \
+      -Dpath="/greeting" \
+      -Dextensions="kotlin,resteasy-jsonb" \
+      -DbuildTool=gradle
