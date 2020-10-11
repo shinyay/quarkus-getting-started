a75639c2b1ba6bea89da3fea228e48a8ff9cf77b
shinyay <shinya.com@gmail.com>
2020-10-11 22:07:19 +0900

    Modify quarkus version as 1.8.3

--- quarkus-create.fish
+++ quarkus-create.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-mvn io.quarkus:quarkus-maven-plugin:1.5.1.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.8.3.Final:create \
       -DprojectGroupId=com.google.shinyay \
       -DprojectArtifactId=quarkus-getting-started \
       -DprojectVersion=0.0.1-SNAPSHOT \
