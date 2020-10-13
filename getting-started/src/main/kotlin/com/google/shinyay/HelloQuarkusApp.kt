983cf1969b896a5c9ee0be8a9d2e49cd87bd4582
shinyay <shinya.com@gmail.com>
2020-10-13 12:19:32 +0900

    Add Quakus with JAX-RS simple hello app

--- getting-started/src/main/kotlin/com/google/shinyay/HelloQuarkusApp.kt
+++ getting-started/src/main/kotlin/com/google/shinyay/HelloQuarkusApp.kt
@@ -1,5 +1,8 @@
 package com.google.shinyay
 
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -10,5 +13,9 @@ class HelloQuarkusApp {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello Quarkus"
+    fun hello(): String {
+        val jstDateTime = ZonedDateTime.now(ZoneId.of("Japan"))
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return "Hello Quarkus at ${jstDateTime.format(formatter)}"
+    }
 }
